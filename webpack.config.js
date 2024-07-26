2c4dfa2301aa7653c3cc77ded7195cdbd31d8f8f
dhruvpurohit2k <purohitdhruv00@gmail.com>
2024-07-26 17:13:52 +0530

    Setup the dev environment. Start making the home page

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { type } = require("os");
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [new HtmlWebpackPlugin({
+    title: "Restaurant",
+    template: "./src/index.html",
+  })],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gig)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      }
+
+    ]
+  }
+}
